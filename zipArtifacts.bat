6bbc63fcaa765c112720fdbebff30818149d153f
jakenjarvis <jaken.jarvis@gmail.com>
2018-09-27 00:14:59 +0900

    Squashed commit of the following:
    commit 1c617e5236026bc0c45c6169edcf772767e1cdbd
    Merge: 80796e90 341a8296
    Author: Masaru Tsuchiyama <m.tmatma@gmail.com>
    Date:   Wed Sep 26 22:12:54 2018 +0900
    
        Merge pull request #484 from m-tmatma/feature/divide-artifacts
    
         実行ファイルと インストーラを別のZIPに分離する
    
    commit 80796e9039019f0b3adb6bde2ca48e449c1032df
    Merge: ec23dc76 05c19a05
    Author: Masaru Tsuchiyama <m.tmatma@gmail.com>
    Date:   Wed Sep 26 21:26:09 2018 +0900
    
        Merge pull request #486 from m-tmatma/feature/fix-artifact-name-with-tag
    
        tag の置換が正しく動かないのを修正
    
    commit 05c19a051824760134a6c8588429d3ee65c1dbf0
    Author: Masaru Tsuchiyama <m.tmatma@gmail.com>
    Date:   Wed Sep 26 20:13:31 2018 +0900
    
        タグの変数に遅延展開を適用
    
    commit 341a82960a536cea86f308646e1f2d2d5b79190f
    Author: Masaru Tsuchiyama <m.tmatma@gmail.com>
    Date:   Wed Sep 26 06:38:07 2018 +0900
    
        すべてのセットに対して、All をつける
    
    commit 43e0041ddaf97c92ea2c7bf4908ee900de83336e
    Author: Masaru Tsuchiyama <m.tmatma@gmail.com>
    Date:   Tue Sep 25 22:42:40 2018 +0900
    
        warning.txt のコピーを後ろに移動
    
    commit ae5c7356e04eb482829ed840a45ce424210b4664
    Author: Masaru Tsuchiyama <m.tmatma@gmail.com>
    Date:   Tue Sep 25 22:34:17 2018 +0900
    
        [ci skip] 誤植を修正
    
    commit b61f6ae623e3e40c2e65b0924e6f056422bcbda0
    Author: Masaru Tsuchiyama <m.tmatma@gmail.com>
    Date:   Tue Sep 25 17:45:40 2018 +0900
    
        appveyor の成果物の説明を追加
    
    commit 80f126d44999d3716185cf211d0e0ffe262457b0
    Author: Masaru Tsuchiyama <m.tmatma@gmail.com>
    Date:   Tue Sep 25 17:37:44 2018 +0900
    
        実行ファイルの ZIP と インストーラの ZIP を分離する

--- zipArtifacts.bat
+++ zipArtifacts.bat
@@ -1,4 +1,5 @@
 @echo off
+@setlocal enabledelayedexpansion
 set platform=%1
 set configuration=%2
 
@@ -52,16 +53,16 @@ if not "%APPVEYOR_BUILD_NUMBER%" == "" (
 @echo checking APPVEYOR_REPO_TAG_NAME %APPVEYOR_REPO_TAG_NAME%
 if not "%APPVEYOR_REPO_TAG_NAME%" == "" (
 	@rem replace '/' with '_'
-	set TEMP_NAME1=%APPVEYOR_REPO_TAG_NAME:/=_%
-	@echo TEMP_NAME1 = %TEMP_NAME1%
+	set TEMP_NAME1=!APPVEYOR_REPO_TAG_NAME:/=_!
+	@echo TEMP_NAME1 = !TEMP_NAME1!
 	
 	@rem replace ' ' with '_'
-	set TEMP_NAME2=%TEMP_NAME1: =_%
-	@echo TEMP_NAME2 = %TEMP_NAME2%
+	set TEMP_NAME2=!TEMP_NAME1: =_!
+	@echo TEMP_NAME2 = !TEMP_NAME2!
 
 	@rem replace ' ' with '_'
-	set TAG_NAME=tag-%TEMP_NAME2%
-	@echo TAG_NAME = %TAG_NAME%
+	set TAG_NAME=tag-!TEMP_NAME2!
+	@echo TAG_NAME = !TEMP_NAME2!
 )
 
 @echo checking APPVEYOR_PULL_REQUEST_NUMBER %APPVEYOR_PULL_REQUEST_NUMBER%
@@ -145,9 +146,11 @@ set WORKDIR_LOG=%WORKDIR%\Log
 set WORKDIR_EXE=%WORKDIR%\EXE
 set WORKDIR_INST=%WORKDIR%\Installer
 set WORKDIR_ASM=%BASENAME%-Asm
-set OUTFILE=%BASENAME%.zip
+set OUTFILE=%BASENAME%-All.zip
 set OUTFILE_LOG=%BASENAME%-Log.zip
 set OUTFILE_ASM=%BASENAME%-Asm.zip
+set OUTFILE_INST=%BASENAME%-Installer.zip
+set OUTFILE_EXE=%BASENAME%-Exe.zip
 
 @rem cleanup for local testing
 if exist "%OUTFILE%" (
@@ -158,6 +161,12 @@ if exist "%OUTFILE_LOG%" (
 )
 if exist "%OUTFILE_ASM%" (
 	del %OUTFILE_ASM%
+)
+if exist "%OUTFILE_INST%" (
+	del %OUTFILE_INST%
+)
+if exist "%OUTFILE_EXE%" (
+	del %OUTFILE_EXE%
 )
 if exist "%WORKDIR%" (
 	rmdir /s /q %WORKDIR%
@@ -185,10 +194,6 @@ copy /Y /B help\macro\macro.chm    %WORKDIR_EXE%\
 copy /Y /B help\plugin\plugin.chm  %WORKDIR_EXE%\
 copy /Y /B help\sakura\sakura.chm  %WORKDIR_EXE%\
 
-copy /Y installer\warning.txt   %WORKDIR%\
-if "%ALPHA%" == "1" (
-	copy /Y installer\warning-alpha.txt   %WORKDIR%\
-)
 copy /Y /B installer\Output-%platform%\*.exe       %WORKDIR_INST%\
 copy /Y msbuild-%platform%-%configuration%.log     %WORKDIR_LOG%\
 copy /Y msbuild-%platform%-%configuration%.log.csv %WORKDIR_LOG%\
@@ -223,9 +228,24 @@ call calc-hash.bat %HASHFILE% %WORKDIR%\
 if exist "%HASHFILE%" (
 	copy /Y %HASHFILE%           %WORKDIR%\
 )
+
+copy /Y installer\warning.txt   %WORKDIR%\
+if "%ALPHA%" == "1" (
+	copy /Y installer\warning-alpha.txt   %WORKDIR%\
+)
 call %ZIP_CMD%       %OUTFILE%      %WORKDIR%
 
 call %ZIP_CMD%       %OUTFILE_LOG%  %WORKDIR_LOG%
+
+@rem copy text files for warning after zipping %OUTFILE% because %WORKDIR% is the parent directory of %WORKDIR_EXE% and %WORKDIR_INST%.
+if "%ALPHA%" == "1" (
+	copy /Y installer\warning-alpha.txt   %WORKDIR_EXE%\
+	copy /Y installer\warning-alpha.txt   %WORKDIR_INST%\
+)
+copy /Y installer\warning.txt        %WORKDIR_EXE%\
+copy /Y installer\warning.txt        %WORKDIR_INST%\
+call %ZIP_CMD%       %OUTFILE_INST%  %WORKDIR_INST%
+call %ZIP_CMD%       %OUTFILE_EXE%   %WORKDIR_EXE%
 
 @echo start zip asm
 mkdir %WORKDIR_ASM%
